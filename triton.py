0dc11cb1e3aefd49fc00fce07cdea9859fb13248
Antonio M. Macías Ojeda <antonio.macias.ojeda@gmail.com>
2015-10-01 21:11:18 +0300

    A bit of cleanup
    - I guess I'll comment it at some point if I'm bored

--- triton.py
+++ triton.py
@@ -6,41 +6,41 @@ def ceildiv(a, b):
     return -(-a // b)
 
 
-def combat(ds, dw, ats, atw):
-    dw += 1
-    df_rounds = ceildiv(ds, atw)
-    surviving_at = ats - (df_rounds * dw)
-    if surviving_at > 0:
-        return "Attacker wins with %i ships remaining" % surviving_at
+def combat(def_ships, def_weapons, att_ships, att_weapons):
+    def_weapons += 1
+    def_rounds = ceildiv(def_ships, att_weapons)
+    surviving_att = att_ships - (def_rounds * def_weapons)
+    if surviving_att > 0:
+        return "Attacker wins with %i ships remaining" % surviving_att
     else:
-        at_rounds = ceildiv(ats, dw)
-        surviving_df = ds - ((at_rounds - 1) * atw)
-        return "Defender wins with %i ships remaining" % surviving_df
+        att_rounds = ceildiv(att_ships, def_weapons)
+        surviving_def = def_ships - ((att_rounds - 1) * att_weapons)
+        return "Defender wins with %i ships remaining" % surviving_def
 
 
-def buy_ind(res, level):
-    return floor(1000. * level / res)
+def buy_ind(star_resources, level):
+    return floor(1000. * level / star_resources)
 
 
-def buy_eco(res, level):
-    return floor(500. * level / res)
+def buy_eco(star_resources, level):
+    return floor(500. * level / star_resources)
 
 
-def buy_sci(res, level):
-    return floor(4000. * level / res)
+def buy_sci(star_resources, level):
+    return floor(4000. * level / star_resources)
 
 
-def buy_warp(res):
-    return floor(10000. / res)
+def buy_warp(star_resources):
+    return floor(10000. / star_resources)
 
 
-def improve_eco(res, start_lvl, end_lvl):
-    return sum(buy_eco(res, _) for _ in range(start_lvl + 1, end_lvl + 1))
+def improve_eco(star_resources, start_lvl, end_lvl):
+    return sum(buy_eco(star_resources, _) for _ in range(start_lvl + 1, end_lvl + 1))
 
 
-def improve_ind(res, start_lvl, end_lvl):
-    return sum(buy_ind(res, _) for _ in range(start_lvl + 1, end_lvl + 1))
+def improve_ind(star_resources, start_lvl, end_lvl):
+    return sum(buy_ind(star_resources, _) for _ in range(start_lvl + 1, end_lvl + 1))
 
 
-def improve_sci(res, start_lvl, end_lvl):
-    return sum(buy_sci(res, _) for _ in range(start_lvl + 1, end_lvl + 1))
+def improve_sci(star_resources, start_lvl, end_lvl):
+    return sum(buy_sci(star_resources, _) for _ in range(start_lvl + 1, end_lvl + 1))
