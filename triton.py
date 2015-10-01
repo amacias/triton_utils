8a434c7597ea7eee0599be73f4cd4c7b239b62fe
Antonio M. Macías Ojeda <antonio.macias.ojeda@gmail.com>
2015-10-01 23:10:27 +0300

    Added ships per hour

--- triton.py
+++ triton.py
@@ -53,3 +53,7 @@ def tech_cost(level):
 def research_time(researched, science, target_lvl):
     hours = (tech_cost(target_lvl) - researched) / science
     print "Current Research ETA: %dd %dh" % (hours // 24, hours % 24)
+
+
+def ships_per_hour(infrastructure, manufacturing=1):
+    return infrastructure * (manufacturing + 5)
